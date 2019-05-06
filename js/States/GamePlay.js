cf59f70fc99d976b1f83af57051e4e10ccddb611
Georgio <gklirono@ucsc.edu>
2019-05-05 17:05:35 -0700

    Cleaned the test scene up

--- js/States/GamePlay.js
+++ js/States/GamePlay.js
@@ -8,14 +8,14 @@ GamePlay.prototype = {
 		
 	},
 	preload: function(){
-
+		// Assets from the example I used
     game.load.image('ball', 'assets/sprites/blue_ball.png');
     game.load.image('background', 'assets/games/starstruck/background2.png');
     game.load.spritesheet('dude', 'assets/games/starstruck/dude.png', 32, 48);
 
 	},
 	create: function(){
-		 this.bg = game.add.tileSprite(0, 0, 1800, 600, 'background');
+		 //this.bg = game.add.tileSprite(0, 0, 1080, 800, 'background');
     this.constraint; // Create the constraint object to be turned on/off
     this.anchored = false; // Create safety switch for anchoring
    // var bounds = new Phaser.Rectangle(190, 100, 200, game.height);
@@ -25,15 +25,6 @@ GamePlay.prototype = {
 	//	Enable p2 physics
 	game.physics.startSystem(Phaser.Physics.P2JS); // Begin the P2 physics
     game.physics.p2.gravity.y = 800; // Add vertical gravity
-    // game.physics.p2.restitution = 0.9;
-
-    /* platforms = game.add.group(); // an attempt to create a wall
-	platforms.enableBody = true;
-
-			//Place a ground platform at the bottom of the level
-	var ground = platforms.create(300, 0, 'ground');
-	ground.scale.setTo(2, 20);
-	ground.body.immovable = true;*/
 
     //  Add 2 sprites which we'll join with a string
 
@@ -52,36 +43,13 @@ GamePlay.prototype = {
 	game.physics.p2.enable(this.player2); // Enable p2 physics for player1. This creates a default rectangular body.
     this.player2.body.fixedRotation = true;// Prevents sprites from spinning wildly
 
-   /* wall = game.add.sprite(400, 400, 'dude');
-   game.physics.p2.enable(wall);
-   // wall.enableBody = true;
-    wall.body.setRectangle(10, 600);
-    wall.body.immovable = true;*/
-
-    //  Lock the two bodies together. The [0, 50] sets the distance apart (y: 80)
-    //var constraint = game.physics.p2.createLockConstraint(sprite2, player, [0, 50], 80);
-
-    /*customBounds = { left: null, right: null, top: null, bottom: null };
-
-    createPreviewBounds(bounds.x, bounds.y, bounds.width, bounds.height);
-	*/
-    //  Just to display the bounds
-   /* var graphics = game.add.graphics(bounds.x, bounds.y);
-    graphics.lineStyle(4, 0xffd900, 1);
-    graphics.drawRect(0, 0, bounds.width, bounds.height);
-    var sim = game.physics.p2;
-    var wall = new p2.Body({ mass: 0, position: [ sim.pxmi(bounds.x), sim.pxmi(bounds.y) ], angle: 1.5707963267 });
-    wall.addShape(new p2.Plane());
-     sim.world.addBody(wall);*/
-
     this.cursors = game.input.keyboard.createCursorKeys(); // Instantiate the cirsor keys to take in input
 	},
 	update: function(){
-		//var hitPlatform = game.physics.p2.collide(player, platforms);
-	var dist = Phaser.Math.difference(this.player2.x, this.player1.x);
-	//sprite2.body.setZeroVelocity();
-	//wall.body.setZeroVelocity();
 
+	var dist = Phaser.Math.difference(this.player2.x, this.player1.x);
+	
+	// Left and right moves player 2
     if (this.cursors.left.isDown)
     {
     	this.player2.body.moveLeft(400);
@@ -91,21 +59,19 @@ GamePlay.prototype = {
     	this.player2.body.moveRight(400);
     }
 
+    //Up and down moves player 1
     if (this.cursors.up.isDown)
     {
-        //sprite2.body.moveUp(400);
         this.player1.body.moveLeft(400);
     }
     else if (this.cursors.down.isDown)
     {
-        //sprite2.body.moveDown(400);
         this.player1.body.moveRight(400);
     }
 
     if(game.input.keyboard.isDown(Phaser.KeyCode.SPACEBAR)) { // spacebar to anchor both players
     	console.log(this.anchored);
     	if(this.anchored == false) {
-    		//this.constraint = game.physics.p2.createLockConstraint(sprite2, player, [0, 50], 80);
     		console.log(dist); // Print the anchor distance
     		this.constraint = game.physics.p2.createDistanceConstraint(this.player2.body, this.player1.body, dist, [0.5,0], [0.5,0]); // Lock the player's x difference
     		this.anchored = true;
@@ -114,7 +80,7 @@ GamePlay.prototype = {
     else {
     	console.log(this.anchored);
     	if(this.anchored == true) {
-    		game.physics.p2.removeConstraint(this.constraint); // Unclock the player's x difference
+    		game.physics.p2.removeConstraint(this.constraint); // Unlock the player's x difference
     		this.anchored = false;
     	}
     }
