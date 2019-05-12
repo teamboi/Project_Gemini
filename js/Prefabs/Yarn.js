845671f7794f3ca0bab39f2f4563b0d3c22d4726
Rookirk <hwu24110@gmail.com>
2019-05-11 18:28:48 -0700

    Added beginnings of another yarn attempt
    added detecting which player is the anchor to updateYarn()

--- js/Prefabs/Yarn.js
+++ js/Prefabs/Yarn.js
@@ -20,32 +20,42 @@ function Yarn(game, key, player1, player2){
 	this.anchored = 0; // 0 = null, 1 = player1, 2 = player2
 
 	// Creates the constraint between the players
-	this.createYarn = function(cat1,cat2){ // First cat will be the anchor
+	this.createYarn = function(anchorCat,cat2){ // First cat will be the anchor
 		this.isYarn = true; // yarn is active
 
-		var dist = Phaser.Math.distance(cat1.x, cat1.y, cat2.x, cat2.y);
+		var dist = Phaser.Math.distance(anchorCat.x, anchorCat.y, cat2.x, cat2.y);
 		this.tautLength = dist; // Sets the taut length
-		cat1.body.data.gravityScale *= 2;
+		anchorCat.body.data.gravityScale *= 2;
 	}
 
 	// Updates the yarn if it is active
 	this.updateYarn = function(){
 		// Only checks if the yarn is active
-		if(this.isYarn == true){
-			var deadband = 3; // the margin of error to check beyond the taut length
-			var dist = Phaser.Math.distance(this.player1.x, this.player1.y, this.player2.x, this.player2.y);
+		if(this.isYarn != true){
+			return;
+		}
+		var deadband = 3; // the margin of error to check beyond the taut length
+		var dist = Phaser.Math.distance(this.player1.x, this.player1.y, this.player2.x, this.player2.y);
 
-			if(dist >= this.tautLength + deadband){ // If the player distance is greater than the taut length, create a constraint
-				if(constraint == null){ // if the constraint doesn't exist already, create a constraint
-					constraint = game.physics.p2.createSpring(this.player1.body, this.player2.body, this.tautLength, 100, 0);
-					//constraint = game.physics.p2.createDistanceConstraint(this.player1.body, this.player2.body, this.tautLength, [0.5,0.5], [0.5,0.5]);
-				}
+		if(this.anchored == 1){
+			var anchorCat = this.player1;
+			var otherCat = this.player2;
+		}
+		else{
+			var anchorCat = this.player2;
+			var otherCat = this.player1;
+		}
+
+		if(dist >= this.tautLength + deadband){ // If the player distance is greater than the taut length, create a constraint
+			if(constraint == null){ // if the constraint doesn't exist already, create a constraint
+				constraint = game.physics.p2.createSpring(this.player1.body, this.player2.body, this.tautLength, 100, 0);
+				//constraint = game.physics.p2.createDistanceConstraint(this.player1.body, this.player2.body, this.tautLength, [0.5,0.5], [0.5,0.5]);
 			}
-			else{ // If the player distance is less than the taut length
-				if(constraint != null){ // If the constraint does exist, remove the constraint
-					game.physics.p2.removeSpring(constraint);
-					constraint = null;
-				}
+		}
+		else{ // If the player distance is less than the taut length
+			if(constraint != null){ // If the constraint does exist, remove the constraint
+				game.physics.p2.removeSpring(constraint);
+				constraint = null;
 			}
 		}
 	}
