b36d19b50a0e94d19d5b9bd29b7dd32f62a71595
Rookirk <hwu24110@gmail.com>
2019-06-06 03:21:21 -0700

    Got window masks to work

--- js/Prefabs/WindowMask.js
+++ js/Prefabs/WindowMask.js
@@ -13,105 +13,36 @@
 // firstY, secondY = range in which the moving platform can move
 // gravityDir = direction of gravity imposed on platform
 // platformType = window or cloud
-function Window(game, gameplay, x, y, key, firstY, secondY, gravityDir){
-	Phaser.Sprite.call(this, game, x, y, key);
-
-	this.scale.setTo(0.22, 0.11); // Scales the sprite
-
-	this.isMoving = false;
-
+function WindowMask(game, gameplay, x, y, windowKey, latchKey, firstY, secondY, gravityDir){
+	Phaser.Sprite.call(this, game, x, y, windowKey);
 	this.gameplay = gameplay;
-	this.platformType = platformType;
 
-	// Enable physics
-	game.physics.p2.enable(this, true);
-	this.body.fixedRotation = true; // Cloud cannot rotate
-	this.body.damping = 0.5;
-	this.body.dynamic = true;
+	this.latch = new MovePlatform(game, gameplay, x, y, latchKey, firstY, secondY, gravityDir);
+	game.add.existing(this.latch);
 
-	if(gravityDir === "down"){
-		this.gravDirMultiplier = 1;
-	}
-	else if(gravityDir === "up"){
-		this.body.data.gravityScale = -1;
-		this.gravDirMultiplier = -1;
-	}
-	else{
-		console.log("Invalid gravity direction");
-	}
+	this.rectMask = game.add.graphics(0, 0);
+	game.add.existing(this.rectMask);
+	this.rectMask.beginFill(0xFFFFFF,1);
 
-	if(this.gravDirMultiplier*firstY < this.gravDirMultiplier*secondY){
-		var maxY = firstY;
-		var minY = secondY;
+	if(gravityDir == "down"){
+		this.anchor.setTo(0.5,1);
+		this.rectMask.drawRect(x - (this.width/2), y-this.height, this.width, this.height);
+	}
+	else if(gravityDir == "up"){
+		this.anchor.setTo(0.5,0);
+		this.rectMask.drawRect(x - (this.width/2), y, this.width, this.height);
 	}
 	else{
-		var maxY = secondY;
-		var minY = firstY;
+		console.log(gravityDir + " is not a valid direction. 'up' or 'down'");
 	}
 
-	var minHeight = y + (this.gravDirMultiplier*this.height);
-	this.min = new MovePlatformLimiter(game, x, minHeight, key);
-	game.add.existing(this.min);
-
-	this.max = new MovePlatformLimiter(game, x, maxY, key);
-	game.add.existing(this.max);
-
-	this.leftLimit = new MovePlatformLimiter(game, x-this.width, y, key);
-	game.add.existing(this.leftLimit);
-
-	this.rightLimit = new MovePlatformLimiter(game, x+this.width, y, key);
-	game.add.existing(this.rightLimit);
-
-	var cloudCG = this.gameplay.cloudCollisionGroup;
-	var limiterCG = this.gameplay.limiterCollisionGroup;
-	var playerCG = this.gameplay.playerCollisionGroup;
-	var surrogateCG = this.gameplay.surrogateCollisionGroup;
-	var objectCG = this.gameplay.objectCollisionGroup
-
-	this.body.setCollisionGroup(cloudCG);
-    this.body.collides([limiterCG, playerCG, surrogateCG, objectCG]);
-
-    this.min.body.setCollisionGroup(limiterCG);
-    this.min.body.collides([cloudCG]);
-
-    this.max.body.setCollisionGroup(limiterCG);
-    this.max.body.collides([cloudCG]);
-
-    this.leftLimit.body.setCollisionGroup(limiterCG);
-    this.leftLimit.body.collides([cloudCG]);
-
-    this.rightLimit.body.setCollisionGroup(limiterCG);
-    this.rightLimit.body.collides([cloudCG]);
-
-    this.deletePlatform = function(){
-    	this.min.destroy();
-    	this.max.destroy();
-    	this.leftLimit.destroy();
-    	this.rightLimit.destroy();
-    	this.destroy();
-    }
+	this.mask = this.rectMask;
 }
 
 // inherit prototype from Phaser.Sprite and set constructor to MovePlatform
-Window.prototype = Object.create(Phaser.Sprite.prototype);
-Window.prototype.constructor = Window;
+WindowMask.prototype = Object.create(Phaser.Sprite.prototype);
+WindowMask.prototype.constructor = Window;
 
-Window.prototype.update = function(){
-	if(this.isMoving === "locked"){
-		return;
-	}
-	else if(this.isMoving === false){
-		if(this.body.velocity.y != 0){
-			this.isMoving = true;
-		}
-	}
-	else{
-		this.leftLimit.updateYPosition(this.body.y);
-		this.rightLimit.updateYPosition(this.body.y);
-		if((this.gravDirMultiplier*this.body.y) - this.height < this.gravDirMultiplier*this.max.body.y){
-			this.min.updateYPosition(this.body.y + (this.gravDirMultiplier*this.height));
-			this.isMoving = "locked";
-			this.static = true;
-		}
-	}
+WindowMask.prototype.update = function(){
+	this.y = this.latch.body.y;
 }
