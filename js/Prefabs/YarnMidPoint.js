b9d1bb7f24c1c6783df68bb1df76434a1ddcfadb
Rookirk <hwu24110@gmail.com>
2019-08-13 22:53:11 -0700

    Renamed some confusing variables

--- js/Prefabs/YarnMidPoint.js
+++ js/Prefabs/YarnMidPoint.js
@@ -6,62 +6,24 @@
 "use strict";
 
 // Constructor for YarnMidPoint
-// For nice curves in the yarn
+// For nice curves in the yarn when not anchoring
 function YarnMidPoint(game, gameplay, player1, player2){
 	Phaser.Sprite.call(this, game, 0, 0, null);
 	game.add.existing(this); //Adds to display list
-
-	this.alpha = 0; // Sets invisible
 
 	this.player1 = player1; // Obtain references to both players
 	this.player2 = player2;
 
 	// When first created, drop the anchor
-	this.dropTween = game.add.tween(this).to( { midAnchorYOffset: 1 }, 1500, Phaser.Easing.Bounce.Out, true, 0, 0, false);
-
-	// Formula used to move this to the midpoint of the players
-	this.calcMidPoint = function(){
-		var xAverage = (this.player1.body.x + this.player2.body.x) / 2;
-		var yAverage = (this.player1.body.y + this.player2.body.y) / 2;
-
-		this.x = xAverage;
-		this.y = yAverage;
-	}
-
-	// Resets the anchor to the midPoint of the players
-	this.resetMidPoint = function(){
-		this.midAnchorYOffset = 0;
-		this.midAnchor.y = this.y;
-	}
-
-	// Causes the anchor to drop
-	this.tweenMidPoint = function(){
-		this.resetMidPoint();
-		this.dropTween.stop();
-		this.dropTween = game.add.tween(this).to( { midAnchorYOffset: 1 }, 1500, Phaser.Easing.Bounce.Out, true, 0, 0, false);
-	}
-
-	// Change whether the anchor should drop upwards or downwards
-	this.changeMidAnchorYMult = function(lastAnchored){
-		if(lastAnchored === this.player1){ // Drop down
-			this.midAnchorYMult = 1;
-		}
-		else if(lastAnchored === this.player2){ //Drop up
-			this.midAnchorYMult = -1;
-		}
-		else{
-			console.log(lastAnchored + " is not a valid player"); // In case of typos
-		}
-	}
+	this.dropTween = game.add.tween(this).to( { midAnchorYDrop: 1 }, 1500, Phaser.Easing.Bounce.Out, true, 0, 0, false);
 
 	// Calculates midpoint so other variables are correct
 	this.calcMidPoint();
 
 	// Creates the anchor with appropriate variables
-	this.midAnchor = game.add.sprite(this.x, this.y, "point");
-	this.midAnchor.alpha = 0;
-	this.midAnchorYOffset = 0;
-	this.midAnchorYMult = 1;
+	this.midAnchor = game.add.sprite(this.x, this.y, null);
+	this.midAnchorYDrop = 0;
+	this.playerGravDir = 1;
 }
 
 // inherit prototype from Phaser.Sprite and set constructor to YarnMidPoint
@@ -71,9 +33,44 @@ YarnMidPoint.prototype.constructor = YarnMidPoint;
 YarnMidPoint.prototype.update = function(){
 	// Calculates midpoint so other variables are correct
 	this.calcMidPoint();
-	// Determines by how much to scale the anchor drop height
-	this.midAnchorYScalar = Phaser.Math.distance(this.player1.x, this.player1.y, this.player2.x, this.player2.y)/400;
+	// Determines by how much to scale the anchor drop height based on player distance
+	this.playerDistYMult = Phaser.Math.distance(this.player1.x, this.player1.y, this.player2.x, this.player2.y)/400;
 
 	this.midAnchor.x = this.x; // anchor matches the midpoint's x
-	this.midAnchor.y = this.y + (125 * this.midAnchorYOffset * this.midAnchorYMult * this.midAnchorYScalar); // acnhor will drop from the midpoint and modify itself according to the distance of the players and who last anchored and how much it has dropped
+	this.midAnchor.y = this.y + (125 * this.midAnchorYDrop * this.playerGravDir * this.playerDistYMult); // acnhor will drop from the midpoint and modify itself according to the distance of the players and who last anchored and how much it has dropped
+}
+
+// Formula used to move this to the midpoint of the players
+YarnMidPoint.prototype.calcMidPoint = function(){
+	var xAverage = (this.player1.body.x + this.player2.body.x) / 2;
+	var yAverage = (this.player1.body.y + this.player2.body.y) / 2;
+
+	this.x = xAverage;
+	this.y = yAverage;
+}
+
+// Change whether the anchor should drop upwards or downwards
+YarnMidPoint.prototype.changeplayerGravDir = function(lastAnchored){
+	if(lastAnchored === this.player1){ // Drop down
+		this.playerGravDir = 1;
+	}
+	else if(lastAnchored === this.player2){ //Drop up
+		this.playerGravDir = -1;
+	}
+	else{
+		console.log(lastAnchored + " is not a valid player"); // In case of typos
+	}
+}
+
+// Resets the anchor to the midPoint of the players
+YarnMidPoint.prototype.resetMidPoint = function(){
+	this.midAnchorYDrop = 0;
+	this.midAnchor.y = this.y;
+}
+
+// Causes the anchor to drop
+YarnMidPoint.prototype.tweenMidPoint = function(){
+	this.resetMidPoint();
+	this.dropTween.stop();
+	this.dropTween = game.add.tween(this).to( { midAnchorYDrop: 1 }, 1500, Phaser.Easing.Bounce.Out, true, 0, 0, false);
 }
