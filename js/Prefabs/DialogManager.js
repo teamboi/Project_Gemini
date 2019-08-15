690e5ce482be1e85b5ead00f0c5f6a62c0467313
Rookirk <hwu24110@gmail.com>
2019-08-14 20:38:46 -0700

    Updated functions to be prototype's functions

--- js/Prefabs/DialogManager.js
+++ js/Prefabs/DialogManager.js
@@ -15,43 +15,43 @@ function DialogManager(game, gameplay){
 	//this.textBubble = this.level[this.textBubbleNum]; // This produces a single text bubble in the level
 
 	this.dialogTyping = false; // Boolean for if the dialog is typing
-
-	// Function to type the intro dialog; 1 indexed
-	this.TypeIntro = function(levelNum){
-		this.intro = this.TypeText(levelNum-1,0);
-	}
-
-	// Function to type the outro dialog; 1 indexed
-	this.TypeOutro = function(levelNum){
-		this.outro = this.TypeText(levelNum-1,1);
-	}
-
-	// Types the text in the array in the json
-	this.TypeText = function(levelNum, textBubbleNum){
-		//this.dialogTyping = true;
-
-		// Obtains reference to the correct text to print
-		var textBubble = this.dialog[levelNum][textBubbleNum];
-
-		// Plays the audio associated with the text
-		/*this.narrate = game.add.audio('narrate');
-        this.narrate.play('', 0, 1, false);
-        this.narrate.volume = 0.35;*/
-
-        // If specified, make the previous text fade out
-		if(textBubble["destroyIntro"]){
-			this.intro.fadeOut();
-		}
-
-		// Create the actual text
-		var currentText = new TextBubble(game, this.gameplay, this.key, textBubble["x"], textBubble["y"], textBubble["width"], textBubble["text"], textBubble["size"]);
-		this.gameplay.group.add(currentText); // Add the current text to be sorted
-		this.gameplay.group.add(currentText.text)
-
-		return currentText; // Returns reference to object
-	}
 }
 
 // inherit prototype from Phaser.Sprite and set constructor to DialogManager
 DialogManager.prototype = Object.create(Phaser.Sprite.prototype);
 DialogManager.prototype.constructor = DialogManager;
+
+// Function to type the intro dialog; 1 indexed
+DialogManager.prototype.TypeIntro = function(levelNum){
+	this.intro = this.TypeText(levelNum-1,0);
+}
+
+// Function to type the outro dialog; 1 indexed
+DialogManager.prototype.TypeOutro = function(levelNum){
+	this.outro = this.TypeText(levelNum-1,1);
+}
+
+// Types the text in the array in the json
+DialogManager.prototype.TypeText = function(levelNum, textBubbleNum){
+	//this.dialogTyping = true;
+
+	// Obtains reference to the correct text to print
+	var textBubble = this.dialog[levelNum][textBubbleNum];
+
+	// Plays the audio associated with the text
+	/*this.narrate = game.add.audio('narrate');
+    this.narrate.play('', 0, 1, false);
+    this.narrate.volume = 0.35;*/
+
+    // If specified, make the previous text fade out
+	if(textBubble["destroyIntro"]){
+		this.intro.fadeOut();
+	}
+
+	// Create the actual text
+	var currentText = new TextBubble(game, this.gameplay, this.key, textBubble["x"], textBubble["y"], textBubble["width"], textBubble["text"], textBubble["size"]);
+	this.gameplay.group.add(currentText); // Add the current text to be sorted
+	this.gameplay.group.add(currentText.text)
+
+	return currentText; // Returns reference to object
+}
