d51de3b5ce255f8ce61b8faf9c8d9118e0d72d90
Rookirk <hwu24110@gmail.com>
2019-08-17 13:30:41 -0700

    Bezier lines now operate off of the visual endpoints rather than exact player coordinates

--- js/Prefabs/Yarn.js
+++ js/Prefabs/Yarn.js
@@ -34,11 +34,16 @@ function Yarn(game, gameplay, player1, player2, surrogate){
 	this.yarnAngle = 0; // Angle of the yarn relative to the anchorCat
 
 	// Adds in the bezier graphics for the yarn
-	// Adds in the bezier handles, respecive of the players
+	// Adds in the bezier handles, respective of the players
 	this.player1BAnchor = game.add.sprite(0, 100, null);
 	this.player2BAnchor = game.add.sprite(100, 0, null);
+	// For debugging, allows to see the bezier handles
+	if(debugCollisionsObjects === true){
+		this.player1BAnchor.sprite = "ball";
+		this.player2BAnchor.sprite = "ball";
+	}
 	this.bezierGraphics = game.add.graphics(0, 0);
-	this.bezierGraphics.z = layerYarn;
+	this.bezierGraphics.zOrder = layerYarn;
 	this.neutralColor = 0x8D58DD; // neutral color of the yarn is purple
 
 	this.gameplay.group.add(this.bezierGraphics); // Adds in the yarn for layer sorting
@@ -192,15 +197,9 @@ Yarn.prototype.createYarn = function(anchorCat,otherCat){ // anchorCat will be t
 }
 
 // Draws the yarn
-Yarn.prototype.drawBezierYarn = function(width, color){
+Yarn.prototype.drawBezierYarn = function(width, color, p1X, p1Y, p2X, p2Y){
 	this.bezierGraphics.clear(); // Clears graphics so we don't see the previous versions of the yarn
 	this.bezierGraphics.lineStyle(width, color, 1); // Sets the style of the line
-
-	// Set the coordinates to draw the strings
-	var p1X = this.player1.x + this.player1.yarnAnchorOffsetX;
-	var p1Y = this.player1.y - this.player1.yarnAnchorOffsetY;
-	var p2X = this.player2.x + this.player2.yarnAnchorOffsetX;
-	var p2Y = this.player2.y + this.player2.yarnAnchorOffsetY;
 
 	this.bezierGraphics.moveTo(p1X,p1Y); // Sets initial position to player1
 	this.bezierGraphics.bezierCurveTo(this.player1BAnchor.x, this.player1BAnchor.y, this.player2BAnchor.x, this.player2BAnchor.y, p2X, p2Y); // Draws the bezier curve to the other player
@@ -208,10 +207,16 @@ Yarn.prototype.drawBezierYarn = function(width, color){
 
 // Draws the yarn as a bezier curve
 Yarn.prototype.drawYarn = function(width, color, anchored){
+	// Set the coordinates to draw the strings
+	var p1X = this.player1.x + this.player1.yarnAnchorOffsetX;
+	var p1Y = this.player1.y - this.player1.yarnAnchorOffsetY;
+	var p2X = this.player2.x + this.player2.yarnAnchorOffsetX;
+	var p2Y = this.player2.y + this.player2.yarnAnchorOffsetY;
+
 	if(anchored === 'taut'){ // If the yarn is in its active state
 		// Obtains the differences between players and sets them to 35% of the way
-		var playerXDiff = (this.player2.body.x - this.player1.body.x)*.35;
-    	var playerYDiff = (this.player2.body.y - this.player1.body.y)*.35;
+		var playerXDiff = (p2X - p1X)*.35;
+    	var playerYDiff = (p2Y - p1Y)*.35;
 
     	// Finds the difference between the current length and the length that the yarn was created at
     	var slackLength = this.tautLength - this.playerDist;
@@ -234,15 +239,15 @@ Yarn.prototype.drawYarn = function(width, color, anchored){
     	}
 
     	// Rotates the bezier handle offsets relative to the string
-    	var handleXOffset = Math.sin(this.yarnAngle)*handleOffsetMult;
-    	var handleYOffset = Math.cos(this.yarnAngle)*handleOffsetMult;
+    	var handleXRotation = Math.sin(this.yarnAngle)*handleOffsetMult;
+    	var handleYRotation = Math.cos(this.yarnAngle)*handleOffsetMult;
 
     	// Sets the bezier handles to the modifiers of everything
-    	this.player1BAnchor.position.setTo(this.player1.x+playerXDiff+handleXOffset, this.player1.y+playerYDiff+handleYOffset);
-    	this.player2BAnchor.position.setTo(this.player2.x-playerXDiff-handleXOffset, this.player2.y-playerYDiff-handleYOffset);
+    	this.player1BAnchor.position.setTo(p1X+playerXDiff+handleXRotation, p1Y+playerYDiff+handleYRotation);
+    	this.player2BAnchor.position.setTo(p2X-playerXDiff-handleXRotation, p2Y-playerYDiff-handleYRotation);
 
     	// Draws the yarn
-    	this.drawBezierYarn(width, color);
+    	this.drawBezierYarn(width, color, p1X, p1Y, p2X, p2Y);
 	}
 	else if(anchored === 'slack'){ // If the yarn is in the inactive state
 		if(this.wasYarnJustReleased === true){ // If the yarn was just released
@@ -255,10 +260,10 @@ Yarn.prototype.drawYarn = function(width, color, anchored){
 			this.midPoint.tweenMidPoint(); // Tells the anchor to drop
 		}
 		// Obtains the distance of each player to the midpoint's anchor
-		var player1XDist = this.midPoint.midAnchor.x - this.player1.x;
-		var player1YDist = this.midPoint.midAnchor.y - this.player1.y;
-		var player2XDist = this.midPoint.midAnchor.x - this.player2.x;
-		var player2YDist = this.midPoint.midAnchor.y - this.player2.y;
+		var player1XDist = this.midPoint.midAnchor.x - p1X;
+		var player1YDist = this.midPoint.midAnchor.y - p1Y;
+		var player2XDist = this.midPoint.midAnchor.x - p2X;
+		var player2YDist = this.midPoint.midAnchor.y - p2Y;
 
 		// By how far back to scale the bezier handles
 		var margin = .2;
@@ -268,7 +273,7 @@ Yarn.prototype.drawYarn = function(width, color, anchored){
     	this.player2BAnchor.position.setTo(this.midPoint.midAnchor.x - player2XDist*margin, this.midPoint.midAnchor.y - player2YDist*margin);
 
     	// Draws the yarn
-		this.drawBezierYarn(width, color);
+		this.drawBezierYarn(width, color, p1X, p1Y, p2X, p2Y);
 	}
 	else{
 		console.log(anchored + " is not a valid state. taut or slack"); // In case I make a typo
