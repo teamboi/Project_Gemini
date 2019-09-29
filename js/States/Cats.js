1c6a3500f6b59b3a87ca55d28ad1d7c9eb76af2a
Rookirk <hwu24110@gmail.com>
2019-09-28 23:34:18 -0700

    Replaced linear easing wiht sinusoidal easing

--- js/States/Cats.js
+++ js/States/Cats.js
@@ -55,13 +55,13 @@ Cats.prototype = {
         // Check for players to be close to each other
         if(Phaser.Math.distance(this.player2.x, this.player2.y, this.player1.x, this.player1.y) < 70){
             this.complete = true;
-            game.add.tween(this.redGlow).to( { alpha: 0.5 }, 100, Phaser.Easing.Linear.None, true, 0);
+            game.add.tween(this.redGlow).to( { alpha: 0.5 }, 100, Phaser.Easing.Sinusoidal.InOut, true, 0);
             this.redGlow.x = (this.player1.x + this.player2.x)/2;
             this.redGlow.y = (this.player1.y + this.player2.y)/2;
         }
         else { 
             this.complete = false;
-            game.add.tween(this.redGlow).to( { alpha: 0 }, 100, Phaser.Easing.Linear.None, true, 0);
+            game.add.tween(this.redGlow).to( { alpha: 0 }, 100, Phaser.Easing.Sinusoidal.InOut, true, 0);
         }
 
 	},
--- js/States/Cradle.js
+++ js/States/Cradle.js
@@ -42,13 +42,13 @@ Cradle.prototype = {
             if(this.player2.anchorState == "isAnchor" || this.player1.anchorState == "isAnchor") {
                 this.complete = true;
             }
-            game.add.tween(this.redGlow).to( { alpha: 0.5 }, 100, Phaser.Easing.Linear.None, true, 0);
+            game.add.tween(this.redGlow).to( { alpha: 0.5 }, 100, Phaser.Easing.Sinusoidal.InOut, true, 0);
             this.redGlow.x = (this.player1.x + this.player2.x)/2;
             this.redGlow.y = (this.player1.y + this.player2.y)/2;
         }
         else { 
             this.complete = false;
-            game.add.tween(this.redGlow).to( { alpha: 0 }, 100, Phaser.Easing.Linear.None, true, 0);
+            game.add.tween(this.redGlow).to( { alpha: 0 }, 100, Phaser.Easing.Sinusoidal.InOut, true, 0);
         }
 
         if(this.oneHasAnchored && this.twoHasAnchored) {
