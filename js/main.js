b7c898ae9ce1b04107bd3939355714048c14b7e4
Rookirk <hwu24110@gmail.com>
2019-11-28 18:05:11 -0800

    reset debug flags

--- js/main.js
+++ js/main.js
@@ -25,9 +25,9 @@ var layerPlayer = 90;
 var debugLevel = null; // default to null; replace with the name of the level to switch to
 var debugCollisionsObjects = false; // true or false; enable collisions with objects
 var debugCollisionsLevel = false; // true or false; enable collisions for the platforms
-var debugLoopLevel = true; // true or false; when a level is completed, loop the level instead of proceeding; only works with levels, not transitions
+var debugLoopLevel = false; // true or false; when a level is completed, loop the level instead of proceeding; only works with levels, not transitions
 var debugAnimation = false; // true or false; prints out animation indices for the player, so I don't have to count, and it isn't calculated every time a level is loaded; Also enables debug on FSM
-var debugTransitions = true; // true or false; makes level transitions instant
+var debugTransitions = false; // true or false; makes level transitions instant
 
 //var levelArr = ['Together', 'Cats', 'Cradle', 'Threads', 'Separate', 'Houses', 'Windows', 'Tether', 'Fences', 'Clouds', 'Ending'];
 
