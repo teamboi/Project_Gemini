f8334cdf0027b234ac008037289fed72b6e06976
Rookirk <hwu24110@gmail.com>
2019-12-19 13:02:01 -0800

    Player can now move directions when yarn is anchored but not taut

--- js/Prefabs/Player.js
+++ js/Prefabs/Player.js
@@ -182,32 +182,36 @@ Player.prototype.move = function(direction){
 	}
 	// ... else if the player is being anchored...
 	else if(!this.checkIfCanJump()){ // If the player is being anchored and hanging in the air
-		var applyForce = true;
+		if(yarn.isTaut === true){
+			var applyForce = true;
 
-		moveDist = Math.sign(moveDist)*this.swingVelocity;
-		//var relativeVel = this.body.velocity.x - this.gameplay.surrogate.body.velocity.x;
-		var force = 1;
+			moveDist = Math.sign(moveDist)*this.swingVelocity;
+			//var relativeVel = this.body.velocity.x - this.gameplay.surrogate.body.velocity.x;
+			var force = 1;
 
-		// If the yarn angle is not vertical ; -1.5 radians is vertical; 3, -3 is blue cat left of right cat
-		// If the yarn is taut
-		if(Math.abs(yarn.yarnAngle + (-0.5 * Math.PI * gs)) > .09 && yarn.isTaut === true){
-			//let scaledAngle = Math.abs( Phaser.Math.mapLinear( yarn.yarnAngle * gs, 0, Math.PI, -1, 1) );
-			//force = Phaser.Math.clamp( Math.abs( 1 / ( 2 * Math.sin( scaledAngle ) + 0.3) ) - 0.5, 0, 1 ); // takes in input 0 - 1
-			//moveDist *= force; // Scales how much the player can move based on the angle of the yarn
-			if(force < .2){
-				applyForce = false;
+			// If the yarn angle is not vertical ; -1.5 radians is vertical; 3, -3 is blue cat left of right cat
+			// If the yarn is taut
+			if(Math.abs(yarn.yarnAngle + (-0.5 * Math.PI * gs)) > .09 && yarn.isTaut === true){
+				//let scaledAngle = Math.abs( Phaser.Math.mapLinear( yarn.yarnAngle * gs, 0, Math.PI, -1, 1) );
+				//force = Phaser.Math.clamp( Math.abs( 1 / ( 2 * Math.sin( scaledAngle ) + 0.3) ) - 0.5, 0, 1 ); // takes in input 0 - 1
+				//moveDist *= force; // Scales how much the player can move based on the angle of the yarn
+				if(force < .2){
+					applyForce = false;
+				}
 			}
+			// If we want to apply the force, move it
+			if(applyForce === true){
+				//this.body.moveRight(moveDist); // Moves the player
+				this.body.force.x += Math.sin(yarn.yarnAngle * gs)*moveDist;
+				this.body.force.y += -gs*Math.abs(Math.cos(yarn.yarnAngle * gs)*moveDist);
+				//console.log(Math.sin(yarn.yarnAngle * gs)*moveDist);
+				//console.log(-gs*Math.abs(Math.cos(yarn.yarnAngle * gs)*moveDist));
+			}	
+			//console.log(moveDist);
 		}
-		// If we want to apply the force, move it
-		if(applyForce === true){
-			//this.body.moveRight(moveDist); // Moves the player
-			this.body.force.x += Math.sin(yarn.yarnAngle * gs)*moveDist;
-			this.body.force.y += -gs*Math.abs(Math.cos(yarn.yarnAngle * gs)*moveDist);
-			console.log(yarn.yarnAngle * gs);
-			//console.log(Math.sin(yarn.yarnAngle * gs)*moveDist);
-			//console.log(-gs*Math.abs(Math.cos(yarn.yarnAngle * gs)*moveDist));
-		}	
-		//console.log(moveDist);
+		else{
+			this.body.moveRight(moveDist);
+		}
 	}
 	else{ // on the ground
 		this.body.moveRight(moveDist);
